8f6f015a954c4f15d1ce50d85a3578585c23ba3d
liangfei0201 <liangfei0201@gmail.com>
2012-12-30 21:42:05 +0800

    1.0.5

--- src/main/java/httl/web/springmvc/HttlViewResolver.java
+++ src/main/java/httl/web/springmvc/HttlViewResolver.java
@@ -39,6 +39,9 @@ public class HttlViewResolver extends AbstractTemplateViewResolver implements In
 
 	public void afterPropertiesSet() throws Exception {
 		WebEngine.setServletContext(getServletContext());
+		if (super.getSuffix() == null) {
+			super.setSuffix(WebEngine.getEngine().getProperty("template.suffix"));
+		}
 	}
 
 }
