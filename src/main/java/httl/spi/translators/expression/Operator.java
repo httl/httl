9722babf8e93a73be37ec741717c3db45ba5876e
liangfei0201 <liangfei0201@gmail.com>
2013-01-01 01:13:56 +0800

    1.0.5

--- src/main/java/httl/spi/translators/expression/Operator.java
+++ src/main/java/httl/spi/translators/expression/Operator.java
@@ -21,9 +21,8 @@ import httl.spi.Translator;
 import java.util.Collection;
 import java.util.Map;
 
-
 /**
- * BranchNode
+ * Operator. (SPI, Prototype, ThreadSafe)
  * 
  * @author Liang Fei (liangfei0201 AT gmail DOT com)
  */
