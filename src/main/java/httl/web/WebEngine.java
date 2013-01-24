a94b12cdc6ae83250b218249612889b3ee6b56cd
liangfei0201 <liangfei0201@gmail.com>
2013-01-24 21:49:59 +0800

    1.0.7

--- src/main/java/httl/web/WebEngine.java
+++ src/main/java/httl/web/WebEngine.java
@@ -68,7 +68,6 @@ public class WebEngine {
 	private WebEngine() {}
 
 	public static void addProperty(String key, String value) {
-		key = key + "+";
 		String old = addProperties.get(key);
 		if (StringUtils.isNotEmpty(old)) {
 			value = old + "," + value;
