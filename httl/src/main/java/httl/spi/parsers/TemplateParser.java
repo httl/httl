336023818ff2e9c01d757632602dcf09758822d5
lveliu <1193041952@qq.com>
2015-06-10 14:06:48 +0800

    解决JS中获取标签ID为end后为大写的错误

--- httl/src/main/java/httl/spi/parsers/TemplateParser.java
+++ httl/src/main/java/httl/spi/parsers/TemplateParser.java
@@ -134,6 +134,10 @@ public class TemplateParser implements Parser {
 			case 'h' : case 'i' : case 'j' : case 'k' : case 'l' : case 'm' : case 'n' : 
 			case 'o' : case 'p' : case 'q' : case 'r' : case 's' : case 't' : 
 			case 'u' : case 'v' : case 'w' : case 'x' : case 'y' : case 'z' :
+			case 'A' : case 'B' : case 'C' : case 'D' : case 'E' : case 'F' : case 'G' :
+			case 'H' : case 'I' : case 'J' : case 'K' : case 'L' : case 'M' : case 'N' :
+			case 'O' : case 'P' : case 'Q' : case 'R' : case 'S' : case 'T' :
+			case 'U' : case 'V' : case 'W' : case 'X' : case 'Y' : case 'Z' :
 				return 1;
 			case '#' : 
 				return 2;
